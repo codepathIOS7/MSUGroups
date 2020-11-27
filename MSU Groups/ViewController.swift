470cf28ae9696993c5acd1cfb8f920126dd482c4
John Lin <john.lin490@gmail.com>
2020-11-27 15:38:32 -0500

    Initial Commit

---
+++ MSU Groups/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSU Groups
+//
+//  Created by John Lin on 11/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
