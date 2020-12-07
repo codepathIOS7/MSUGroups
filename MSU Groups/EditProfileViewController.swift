4e4945bff935456ff72fe75f64e7089aa683d8a0
John Lin <john.lin490@gmail.com>
2020-12-07 16:07:26 -0500

    Edit profile updates

---
+++ MSU Groups/EditProfileViewController.swift
@@ -0,0 +1,56 @@
+//
+//  EditProfileViewController.swift
+//  MSU Groups
+//
+//  Created by John Lin on 12/7/20.
+//
+
+import UIKit
+import Parse
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet weak var majorTextField: UITextField!
+    
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onSaveChanges(_ sender: Any) {
+        let user = PFUser.current
+        user()!["major"] = majorTextField.text
+        user()!["year"] = yearTextField.text
+      //  let profile = PFObject(className:"Profile")
+      //  profile["major"] = majorTextField.text
+       // profile["year"] = yearTextField.text
+
+        // Saves the new object.
+        user()!.saveInBackground {
+          (success: Bool, error: Error?) in
+          if (success) {
+            print("Success!!")
+          } else {
+            print("Error!!")
+          }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
